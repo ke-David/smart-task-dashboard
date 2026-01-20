6994cee158e3b0f64f0dac9d13ca8211c3ccc628
KedvesDavid <kedves.david@student.ms.sapientia.ro>
2026-01-20 20:42:22 +0200

    Connect frontend to backend via REST API, add tasks.JSON for data persistence

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,20 @@
- 
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+tasks = []
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def add_tasks():
+    data = request.get_json()
+    tasks.append(data)
+    return jsonify(data), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- frontend/script.js
+++ frontend/script.js
@@ -5,39 +5,83 @@ const taskList = document.getElementById("task-list");
 
 let tasks = [];
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
+//load tasks
+document.addEventListener("DOMContentLoaded", loadTasks);
 
-    const taskText = taskInput.value.trim();
-    const category = categorySelect.value;
-
-    if (taskText === "") return;
-
-    const task = {
-        text: taskText,
-        category: category
-    };
-
-    tasks.push(task);
+async function loadTasks(){
+    const res = await fetch("http://127.0.0.1:5000/tasks");
+    tasks = await res.json();
     renderTasks();
+}
 
-    taskInput.value = "";
-});
-
-function renderTasks() {
+function renderTasks(){
     taskList.innerHTML = "";
 
-    function renderTask(task) {
-        const li = document.createElement("li");
-        li.textContent = `${task.text} (${task.category})`; //template literal, can put any JS expression, not just variables
-        taskList.appendChild(li);
-    }
+    tasks.forEach(task => renderTask(task));
+}
 
-    tasks.forEach(function (task){  //anonymous func
-        const li = document.createElement("li");
-        li.textContent = `${task.text} (${task.category})`;
-        taskList.appendChild(li); //only accepts element made by document.createElement, li is a Node object
+function renderTask(task){
+    const li = document.createElement("li");
+    li.textContent = `${task.text} (${task.category})`;
+
+    taskList.appendChild(li);
+}
+
+form.addEventListener ("submit", async (event) => {
+    event.preventDefault();
+
+    const text = taskInput.value.trim();
+    const category = categorySelect.value;
+    
+    if (text === "") return;
+
+    const task = {text, category};
+
+    await fetch("http://127.0.0.1:5000/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(task),
     });
 
-}
+    taskInput.value = "";
+    loadTasks();
+})
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     const taskText = taskInput.value.trim();
+//     const category = categorySelect.value;
+
+//     if (taskText === "") return;
+
+//     const task = {
+//         text: taskText,
+//         category: category
+//     };
+
+//     tasks.push(task);
+//     renderTasks();
+
+//     taskInput.value = "";
+// });
+
+// function renderTasks() {
+//     taskList.innerHTML = "";
+
+//     function renderTask(task) {
+//         const li = document.createElement("li");
+//         li.textContent = `${task.text} (${task.category})`; //template literal, can put any JS expression, not just variables
+//         taskList.appendChild(li);
+//     }
+
+//     tasks.forEach(function (task){  //anonymous func
+//         const li = document.createElement("li");
+//         li.textContent = `${task.text} (${task.category})`;
+//         taskList.appendChild(li); //only accepts element made by document.createElement, li is a Node object
+//     });
+
+// }
+
+
  
